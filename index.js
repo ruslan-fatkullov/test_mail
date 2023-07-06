c3397c82e5ff3befa64e978cee30e5f68e88ad0b
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-07-06 10:37:12 +0400

    git init

--- index.js
+++ index.js
@@ -12,15 +12,43 @@ app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`)
 });
 
-app.get("/", function(request, response)  {
+app.get("/", function (request, response) {
     response.render("homePage");
 });
 
 
 
 
+const nodemailer = require('nodemailer');
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     console.log(req.query.email)
-    res.json({message: "Вы зарегистрировались"})
+    res.json({ message: "Вы зарегистрировались" })
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.forwardemail.net",
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'root@get-esvo-launcher.ru',
+            pass: 'dsfdsfdsfgh'
+        }
+    });
+
+    async function main() {
+        const info = await transporter.sendMail({
+            from: '"Fred Foo 👻" <root@get-esvo-launcher.ru>', // sender address
+            to: req.query.email,
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+
+    }
+
+    main().catch(console.error);
+
+
 });
